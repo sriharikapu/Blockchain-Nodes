5498c26baa0e5d42e1abe84cacfb5d57e0dcdcd9
srihari kapu <sriharikapu@mail.com>
2019-03-07 10:01:23 +0800

    Create litecoin-testnet.sh

---
+++ litecoin-testnet.sh
@@ -0,0 +1,209 @@
+    1  pacman -Syu
+    2  sudo apt install pacman
+    3  sudo apt-get update
+    4  sudo apt install pacman
+    5  yaourt
+    6  git clone https://github.com/litecoin-project/litecoin.git
+    7  cd litecoin/
+    8  yaourt
+    9  sudo pacman -Sy yaourt
+   10  cd ..
+   11  sudo nano /etc/pacman.conf
+   12  sudo pacman -Sy yaourt
+   13  sudo pacman -S --needed base-devel git wget yajl
+   14  sudo apt install pacman
+   15  sudo pacman -Sy yaourt
+   16  pacman
+   17  pacman -Sy
+   18  pacman -Sy yaourt
+   19  git clone https://aur.archlinux.org/package-query.git
+   20  cd package-query/
+   21  makepkg -si
+   22  ls
+   23  make pkg -si
+   24  sudo apt install make
+   25  makepkg -si
+   26  sudo apt install makepkg
+   27  ls
+   28  make
+   29  make PKGBUILD
+   30  apt-get update
+   31  sudo apt-get update
+   32  git clone https://github.com/litecoin-project/litecoin.git
+   33  ls
+   34  cd litecoin
+   35  ls
+   36  make info
+   37  cd
+   38  make info
+   39  make start
+   40  sudo make start
+   41  cd litecoine
+   42  cd litecoin
+   43  litecoind
+   44  ./autogen.sh
+   45  sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev
+   46  sudo apt-get install software-properties-common
+   47  sudo add-apt-repository ppa:bitcoin/bitcoin
+   48  sudo apt-get update
+   49  sudo apt-get install libdb4.8-dev libdb4.8++-dev
+   50  ./autogen.sh
+   51  ./configure
+   52  make
+   53  litecoind
+   54  make install
+   55  litecoind getinfo
+   56  make start
+   57  make getinfo
+   58  litecoind
+   59  ls
+   60  nano litecoin.conf
+   61  litecoind
+   62  cd
+   63  ls
+   64  litecoind
+   65  cd litecoin
+   66  cd src
+   67  strip litecoind
+   68  adduser litecoin
+   69  sudo adduser litecoin
+   70  su litecoin
+   71  ls
+   72  cd litecoin
+   73  make getinfo
+   74  ls
+   75  cd src
+   76  make getinfo
+   77  litecoind
+   78  cd ..
+   79  litecoind
+   80  ./litecoin-cli help
+   81  litecoind
+   82  litecoind-daemon
+   83  ~/litecoin-0.8.7.1-linux/bin/64/litecoind getinfo
+   84  litecoind
+   85  ls
+   86  cd litecoin
+   87  litecoind
+   88  ./configure
+   89  litecoind
+   90  ls
+   91  make
+   92  make getinfo
+   93  make install
+   94  make info
+   95  litecoin-cli
+   96  litecoind
+   97  cd src
+   98  litecoind
+   99  ./litecoind
+  100  ./litecoin-cli help
+  101  litecoind
+  102  cd litecoin
+  103  chmod 0600 litecoin.conf
+  104  cd ../bin
+  105  litecoind -daemon
+  106  ./litecoind -daemon
+  107  ls
+  108  cd
+  109  cd /.litecoin
+  110  cd /home/ubuntu/.litecoin/
+  111  nano litecoin.conf
+  112  chmod 0600 litecoin.conf
+  113  cd ../bin
+  114  litecoind -daemon
+  115  ./litecoin-cli getblockchaininfo
+  116  ls
+  117  nano litecoin.config
+  118  cd
+  119  cd litecoin
+  120  ls
+  121  nano litecoin.config
+  122  ls
+  123  litecoin
+  124  cd litecoin
+  125  litecoind
+  126  litecoind -daemon
+  127  help
+  128  ls
+  129  litecoind -daemon
+  130  cd src/
+  131  litecoind -daemon
+  132  ls
+  133  ./litecoind -daemon
+  134  pwd
+  135  crontab -e 
+  136  litecoin-cli
+  137  ./litecoin-cli 
+  138  ./litecoin-cli getblockchaininfo
+  139  ./litecoin-cli -?
+  140  ./litecoin-cli rpcuser=admin
+  141  nano litecoin.conf
+  142  ./litecoin-cli getblockchaininfo
+  143  nano litecoin.conf
+  144  cd
+  145  cd litecoin
+  146  ls
+  147  nano litecoin.conf
+  148  ./litecoin-cli getblockchaininfo
+  149  cd src
+  150  ./litecoin-cli getblockchaininfo
+  151  nano litecoin.conf
+  152  ls
+  153  litecoind
+  154  ./litecoind
+  155  ./litecoind -getinfo
+  156  ./litecoin-cli getblockchaininfo
+  157  ./litecoind getblockchaininfo
+  158  litecoind -h
+  159  ./litecoin-cli getblockchaininfo
+  160  ./litecoin-cli daemon
+  161  ./litecoin-cli -?
+  162  litecoind
+  163  ./litecoin-cli getnewaddress admin
+  164  ./litecoin-cli getblockchaininfo
+  165  ~/git $ litecoin/src/litecoind getblockcount
+  166  ./litecoin-cli getblockchaininfo
+  167  ls
+  168  cd litecoin/
+  169  ls
+  170  cd src/
+  171  ./litecoin-cli -?
+  172  ./litecoin-cli -getinfo
+  173  sudo su
+  174  ./litecoin-cli -getinfo
+  175  sudo ./litecoin-cli -getinfo
+  176  sudo ./litecoins -daemon
+  177  ./litecoind -daemon
+  178  ./litecoin-cli -getinfo
+  179  sudo ./litecoin-cli -getinfo
+  180  ./litecoind -daemon
+  181  ./litecoin-cli -testnet -rpcuser=123456 -rpcpassword=1234564 getbalance
+  182  ls
+  183  sudo nano litecoin.conf 
+  184  ./litecoin-cli -testnet -rpcuser=admin -rpcpassword=1234564 getbalance
+  185  ./litecoin-cli -?
+  186  ./litecoin-cli -testnet -rpcuser=admin -rpcpassword=1234564 getbalance
+  187  sudo nano litecoin.conf 
+  188  sudo nano litecoin.json
+  189  ./litecoin-cli -testnet -rpcuser=admin -rpcpassword=1234564 getbalance server=1
+  190  ./litecoind -daemon
+  191  ./litecoin-cli -testnet -rpcuser=admin -rpcpassword=1234564 getbalance server=1
+  192  ps -ax grep||litecoin
+  193  ./litecoin-cli -testnet -rpcuser=admin -rpcpassword=1234564 getbalance server=1
+  194  sudo nano litecoin.conf 
+  195  ./litecoind -daemon
+  196  ./litecoin-cli -testnet -rpcuser=admin -rpcpassword=1234564 getbalance server=1
+  197  sudo nano litecoin.conf 
+  198  ./litecoind -daemon
+  199  ./litecoin-cli -testnet -rpcuser=admin -rpcpassword=1234564 getbalance server=1
+  200  crontab -e
+  201  ./litecoind -daemon -testnet
+  202  crontab -e
+  203  ./litecoind -daemon -testnet
+  204  ./litecoin-cli -testnet -rpcuser=admin -rpcpassword=1234564 getbalance server=1
+  205  ./litecoin-cli -testnet -rpcuser=admin -rpcpassword=1234564 getbalance 
+  206  ./litecoin-cli -testnet -?
+  207  ./litecoin-cli -testnet -version
+  208  ./litecoin-cli -testnet -rpcport
+  209  ./litecoin-cli -testnet -getinfo
